4e7e158c8365bcd362d828c530bb094c12654e75
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-16 13:52:13 -0400

    loop

--- loop/for.py
+++ loop/for.py
@@ -9,10 +9,50 @@ color = ['red','black', 'green']
 # for t in color:
 #     print(t)
 
-r = color[0]
-print(r)
-for d in r:
-    print(d)
+# r = color[0]
+# print(r)
+# for d in r:
+#     print(d)
 
-for y in "Abir":
-    print("String the loop " + str(y))
+# for y in "Abir":
+#     print("String the loop " + str(y))
+
+
+# Break
+# for x in color:
+#     if x == "black":
+#         break
+#     print("Break " + str(x))
+
+# for c in color:
+#     if c == "black":
+#         continue
+#     print("Continue: " + str(c))
+# specify the starting the value
+# for a in range(2, 8):
+#     print(a)
+
+# for x in range(1,10,2):
+#     print(x)
+
+# else
+# for x in range(1,8):
+#     if x == 5:
+#         continue
+#     print(x)
+# else:
+#     print("Finished")
+
+# def sum_squares(n):
+#     sum = 0
+#     for c in range(n):
+#         sum += c * c
+#     return sum
+print(sum_squares(10))
+
+# total and find the length
+
+num = [4, 6, 7, 8]
+
+sum1 = 0
+lenght = 0
