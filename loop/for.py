74b1a70c62817e7767749e3da493db8057d3b8b5
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-16 12:15:35 -0400

    loop

---
+++ loop/for.py
@@ -0,0 +1,10 @@
+for c in range(5):
+    print("execute from 0 to 4 = " + str(c))
+
+for x in range(5):
+    print("execute from 1 to 5 = " + str(x +1))
+
+color = ['red','black', 'green']
+
+for t in color:
+    print(t)
