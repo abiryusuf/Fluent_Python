2b4c3ad4b5d593b8b9836b57fe89b511c0a10ab2
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-16 12:41:57 -0400

    string

--- loop/for.py
+++ loop/for.py
@@ -1,10 +1,18 @@
-for c in range(5):
-    print("execute from 0 to 4 = " + str(c))
+# for c in range(5):
+#     print("execute from 0 to 4 = " + str(c))
 
-for x in range(5):
-    print("execute from 1 to 5 = " + str(x +1))
+# for x in range(5):
+#     print("execute from 1 to 5 = " + str(x +1))
 
 color = ['red','black', 'green']
 
-for t in color:
-    print(t)
+# for t in color:
+#     print(t)
+
+r = color[0]
+print(r)
+for d in r:
+    print(d)
+
+for y in "Abir":
+    print("String the loop " + str(y))
