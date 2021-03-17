89fd10f8b6689a6daefbe4efadb21d139acdafdb
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-17 18:03:21 -0400

    list

---
+++ list/listDemo.py
@@ -0,0 +1,9 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles[0].title())
+
+message = "My first bicyle was a " + bicycles[0].title() + "."
+print(message)
+
+for x in bicycles:
+    print(x[0].title() + x[1].title())
