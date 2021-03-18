a079c4b77ef96b0b48744d32e3123820ab92b3db
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-17 22:57:59 -0400

    list comprehensions

--- list/listDemo.py
+++ list/listDemo.py
@@ -1,13 +1,56 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 
-print(bicycles[0].title())
+# print(bicycles[0].title())
 
-message = "My first bicyle was a " + bicycles[0].title() + "."
-print(message)
+# message = "My first bicyle was a " + bicycles[0].title() + "."
+# print(message)
 
-for x in bicycles:
-    print(x[0].title() + x[1].title())
+# for x in bicycles:
+#     print(x[0].title() + x[1].title())
 
 
-bicycles.append("car")
-print(bicycles[-1].title())
+# bicycles.append("car")
+# print(bicycles[-1].title())
+
+# bicycles.insert(2, "honda")
+# print(bicycles)
+
+# bicycles[2] = 'toyota'
+# print(bicycles)
+
+# del bicycles[0]
+# print(bicycles)
+
+# bicycles.append("trek")
+# print(bicycles)
+
+# bicycles.pop(0)
+# print(bicycles)
+
+# too_expensive = 'honda'
+# bicycles.remove(too_expensive)
+# print(too_expensive)
+# print("\nA " + too_expensive.title() + ' is too expensive for me. ')
+
+# sort 
+# maintain the order based on the letter
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort()
+# print(cars)
+
+# cars.sort(reverse=True)
+# print(cars)
+
+# Range 
+thisList = ["apple", "banana", "cherry", "orange", "melon", "mango"]
+
+y = thisList[1:3]
+
+print(y)
+
+t = "apple" in thisList
+print(t)
+
+#
+z = [x * 2 for x in range(1, 20, 2)]
+print(z)
