2ff59979dd142e5abcc6616024020ecd2d83db7f
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-19 11:32:25 -0400

    list quiz

--- Chapter_4/list_looping.py
+++ Chapter_4/list_looping.py
@@ -1,13 +1,21 @@
-# magicians = ['Alice', 'David', 'Carolina']
+magicians = ['Alice', 'David', 'Carolina']
 
-# for magician in magicians:
-#     print(magician.title() + ", that was a great trick")
-#     print("I can not wait to see your next trick, " + magician.title() + ".\n")
-# print('Thank You')
+for magician in magicians:
+    print(magician.title() + ", that was a great trick")
+    print("I can not wait to see your next trick, " + magician.title() + ".\n")
+print('Thank You')
 
-names = ["Abir", 'Yusuf', 'Mim']
+# names = ["Abir", 'Yusuf', 'Mim']
 
-for name in names:
-    print(name)
-    print('My name is ' + name.title() + ".\n")
-print("Name")
+# for name in names:
+#     print(name)
+#     print('My name is ' + name.title() + ".\n")
+# print("Name")
+
+# msg = 'Hello Pyhton'
+# print(msg)
+
+animals = ['cat', 'dog', 'cow']
+for animal in animals:
+    print("A " + animal.title() + " would make a great pet")
+    print("Any of these animals would make great pet.\n")
