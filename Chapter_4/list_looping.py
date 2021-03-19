1142ebdc142da785eedc3220625aee55a6df182b
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-19 11:02:25 -0400

    indent error

--- Chapter_4/list_looping.py
+++ Chapter_4/list_looping.py
@@ -1,5 +1,13 @@
-magicians = ['Alice', 'David', 'Carolina']
+# magicians = ['Alice', 'David', 'Carolina']
 
-for magician in magicians:
-    # print(magician.title() + ", that was a great trick")
-    print("I can not wait to see your next trick, " + magician.title() + ".\n")
+# for magician in magicians:
+#     print(magician.title() + ", that was a great trick")
+#     print("I can not wait to see your next trick, " + magician.title() + ".\n")
+# print('Thank You')
+
+names = ["Abir", 'Yusuf', 'Mim']
+
+for name in names:
+    print(name)
+    print('My name is ' + name.title() + ".\n")
+print("Name")
