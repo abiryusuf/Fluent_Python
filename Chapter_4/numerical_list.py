ff70fe7b08d9ef0d08ef333d7e008a0ad020b159
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-20 13:41:43 -0400

    list update

--- Chapter_4/numerical_list.py
+++ Chapter_4/numerical_list.py
@@ -1,8 +1,15 @@
 
 for value in range(1, 5 +1):
     print(value)
-def myFun(n):
-    for x in range(1,n -1):
-        print(x)
+# def myFun(n):
+#     for x in range(1,n -1):
+#         print(x)
 
-myFun(6)
+# myFun(6)
+for x in list(range(1,5)):
+    print(x)
+print(type(x))
+
+# even number 
+even_number = list(range(2,11,2))
+print(even_number)
