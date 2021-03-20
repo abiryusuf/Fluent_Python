fe64642a32fb1308a7c0039de9d21703a8b3556a
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-20 14:00:16 -0400

    update

--- Chapter_4/numerical_list.py
+++ Chapter_4/numerical_list.py
@@ -11,5 +11,21 @@ for x in list(range(1,5)):
 print(type(x))
 
 # even number 
-even_number = list(range(2,11,2))
-print(even_number)
+# even_number = list(range(2,11,2))
+# print(even_number)
+
+# square
+
+squares = []
+
+for value in range(1,11):
+    squre = value * 2
+    squares.append(squre)
+print(squares)
+
+def function(n):
+    squares = []
+    for value in range(1, n +1):
+        squares.append(value *2)
+    return squares
+print(function(6))
