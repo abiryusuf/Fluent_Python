fb21d97eeaecb22b9f814e580b6de909b01efefb
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-21 13:27:25 -0400

    qize

--- Chapter_4/copy_list.py
+++ Chapter_4/copy_list.py
@@ -8,5 +8,13 @@ print(my_foods)
 
 print("\nMy friend's favorite foods are: ")
 friends_food = foods[2:]
+# add new item by insert method that will add with index number
 friends_food.insert(1, "ice cream")
 print(friends_food)
+
+# try it yourself 
+languages = ['java', 'python', 'C++', 'javaScript', 'C', 'ruby']
+
+print(languages[:3])
+
+print(languages[2:4])
