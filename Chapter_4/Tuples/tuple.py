4a14c6d564d39339f0610c1a796e48e096505c02
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-22 21:49:26 -0400

    overwriting tuple

--- Chapter_4/Tuples/tuple.py
+++ Chapter_4/Tuples/tuple.py
@@ -14,3 +14,16 @@ print('\nModified dimensions: ')
 for dimension in dimensions:
     print(dimension)
 
+# try it yorself
+print('orginal buffet: ')
+buffet_style = ('a', 'b', 'c', 'd', 'e')
+for style in buffet_style:
+    print(style)
+
+# buffet_style[0] = 'f'
+# print(buffet_style)
+
+buffet_style =('a', 'b', 'c', 'f', 'g')
+print('\nReplacing two items: ')
+for newItem in buffet_style:
+    print(newItem)
