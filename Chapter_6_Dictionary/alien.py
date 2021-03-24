2190e0017a6965f38e7cdc94af2a8f3b6cb4fd64
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-24 19:02:38 -0400

    dictionary

--- Chapter_6_Dictionary/alien.py
+++ Chapter_6_Dictionary/alien.py
@@ -7,6 +7,23 @@ print(aline_0)
 # index 
 x = aline_0['color']
 print(x)
+ # add
+aline_0["age"] = 32
+aline_0['Place'] = "NY"
+print(aline_0)
+
+# change 
+aline_0['Place'] = 'Florida'
+print(aline_0)
+
+del aline_0['Place']
+print(aline_0)
+
+
+
 
 new_points = aline_0['point']
 print('you just earend '  + str(new_points) +" points")
+
+for dic in aline_0.items():
+    print(dic)
