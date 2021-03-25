dee26b0ba73340926a4ae019060c18320f090b31
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-24 21:36:55 -0400

    quiz

--- Chapter_6_Dictionary/alien.py
+++ Chapter_6_Dictionary/alien.py
@@ -1,29 +1,64 @@
-aline_0 = {
-    'color': 'green',
-    'point': 5
+# aline_0 = {
+#     'color': 'green',
+#     'point': 5
+# }
+# print(aline_0)
+
+# # index 
+# x = aline_0['color']
+# print(x)
+#  # add
+# aline_0["age"] = 32
+# aline_0['Place'] = "NY"
+# print(aline_0)
+
+# # change 
+# aline_0['Place'] = 'Florida'
+# print(aline_0)
+
+# del aline_0['Place']
+# print(aline_0)
+
+# file_count ={}
+
+# file_count['jpg'] = 10
+# file_count['text'] = 14
+# file_count['cvs'] = 2
+# file_count['py'] = 33
+
+# # modify
+# x = file_count ['cvs'] = 3
+# print("the cvs is now {} before it was 2, ".format(x))
+
+alien = {
+    'x_position': 0,
+    'y_position': 25,
+    'speed': 'medium'
 }
-print(aline_0)
+print('Orginial x-position: ' + str(alien['x_position']))
 
-# index 
-x = aline_0['color']
-print(x)
- # add
-aline_0["age"] = 32
-aline_0['Place'] = "NY"
-print(aline_0)
+if alien['speed'] == 'slow':
+    x_increment = 1
+elif alien['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
 
-# change 
-aline_0['Place'] = 'Florida'
-print(aline_0)
+alien['x_position'] = alien['x_position'] + x_increment
+print('New x-postion: ' + str(alien['x_position']))
 
-del aline_0['Place']
-print(aline_0)
+# print('you just earend '  + str(new_points) +" points")
 
+# for dic in aline_0.items():
+#     print(dic)
 
-
-
-new_points = aline_0['point']
-print('you just earend '  + str(new_points) +" points")
-
-for dic in aline_0.items():
-    print(dic)
+# try it yourself
+person = {
+    "first_name": "Abir",
+    'last_name': 'Yusuf',
+    "Age": 32,
+    "City": "NY"
+}
+print(person)
+for info in person.items():
+    print(info)
