eaa6fa6ef7539bb88e01ce22b95ad149bbce988b
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-26 14:31:49 -0400

    nesting

---
+++ Chapter_6_Dictionary/nesting.py
@@ -0,0 +1,8 @@
+alien_0 = {'color': 'green', 'points': 5} 
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_2, alien_1, alien_0]
+
+for alien in aliens:
+    print(alien)
