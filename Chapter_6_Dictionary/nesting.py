fb230d00efaefb6b005eef1b6f27f32c2108f89a
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-26 14:47:44 -0400

    update

--- Chapter_6_Dictionary/nesting.py
+++ Chapter_6_Dictionary/nesting.py
@@ -6,3 +6,11 @@ aliens = [alien_2, alien_1, alien_0]
 
 for alien in aliens:
     print(alien)
+
+aliens_Empty = []
+for alien in range(30):
+    new_alien = {'color': 'green', 'point': 5, 'speed': 'slow'}
+    aliens_Empty.append(new_alien)
+
+for alien in aliens[:5]:
+    print("number" + str(alien))
