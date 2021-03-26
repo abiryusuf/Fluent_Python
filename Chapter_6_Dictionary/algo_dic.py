4ad10945a6869afe3ebeb863b3b717190df659b5
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-25 23:16:39 -0400

    update

--- Chapter_6_Dictionary/algo_dic.py
+++ Chapter_6_Dictionary/algo_dic.py
@@ -1,5 +1,5 @@
 # count letters from sentences
-
+# 1
 def count_letters(text):
     result = {}
     for letter in text:
@@ -10,6 +10,7 @@ def count_letters(text):
 
 print(count_letters('I am abir'))
 
+# 2
 groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59, 
 	"coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
 
@@ -20,3 +21,17 @@ def total_price(basket):
     return round(total,2)
 
 print("Total price - {}".format(total_price(groceries)))
+# 3
+add_email = {
+    "gmail.com": ["clark.kent", "diana.prince", "peter.parker"], 
+    "yahoo.com": ["barbara.gordon", "jean.grey"], 
+    "hotmail.com": ["bruce.wayne"]
+    }
+
+def email_list(domains):
+    email = []
+    for domain, users in domains.items():
+        for user in users:
+            email.append(user + "@" + domain)
+    return email
+print(email_list(add_email))
